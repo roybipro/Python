c0b29d15693a78cb8c4920604d63e4fa0757ab3c
Bipro Roy <roybipro896@gmail.com>
2025-05-10 13:12:38 +0600

    add a matplotlib code

---
+++ Matplotlib/mat.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.title("Sine Wave")
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+plt.grid(True)
+plt.show()
